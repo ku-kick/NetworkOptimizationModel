872aa5e9524f536d1459a514aa52e89ad25c48e6
DmitryMurashov <dmtr.murashov@gmail.com>
2022-07-31 16:14:57 +0300

    [test:test_linsmat] Impl

--- test/test_linsmat.py
+++ test/test_linsmat.py
@@ -2,6 +2,7 @@ import unittest
 import pathlib
 import sys
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent))
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / 'twoopt'))
 from twoopt import linsmat
 
 
@@ -9,7 +10,16 @@ class TestArgs(unittest.TestCase):
 
 	def test_post_init(self):
 		ind = linsmat.RowIndex(indices=dict(j=2, rho=3), variables=dict(x=['j', 'rho'], y=['j']))
-		print(ind.radix_map)
 
+	def test_to_mixed_radix_num(self):
+		ind = linsmat.RowIndex(indices=dict(a=3, b=5), variables=dict(x=['a'], y=['a', 'b']))
+
+	def test_get_pos(self):
+		ind = linsmat.RowIndex(indices=dict(a=3, b=5), variables=dict(x=['a'], y=['a', 'b'], z=['a', 'b']))
+		print(ind.get_pos('y', a=2, b=4))
+		print(ind.get_pos('x', a=2))
+		print(ind.radix_maps)
+		print(ind.radix_mult_vectors)
+		print(linsmat.RowIndex(indices=dict(a=3), variables=dict(x=['a'])).get_pos('x', a=2))
 
 unittest.main()
