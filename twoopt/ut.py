4938e101a99aa4a988703bc7dcba5be3cb2e0cfc
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-13 12:03:36 +0300

    [twoopt:ut] Impl | Date / time op-s

--- twoopt/ut.py
+++ twoopt/ut.py
@@ -1,3 +1,8 @@
+import itertools
+import datetime
+from dateutil import parser as date_parser
+import os
+
 
 def iter_plain(root):
 	try:
@@ -5,3 +10,39 @@ def iter_plain(root):
 			yield from iter_plain(i)
 	except TypeError:
 		yield root
+
+
+def radix_cartesian_product(radix_boundaries):
+	mapped = map(range, radix_boundaries)
+	return itertools.product(*mapped)
+
+
+def file_create_if_not_exists(filename):
+	if not os.path.exists(filename):
+		with open(filename, 'w'):
+			pass
+
+
+class Datetime:
+	DATE_FORMAT = "%Y-%m-%d"
+	TIME_FORMAT = "%Y%m%d%H%M"
+
+	@staticmethod
+	def today():
+		return datetime.datetime.now()
+
+	@staticmethod
+	def yesterday():
+		return datetime.datetime.now() - datetime.timedelta(days=1)
+
+	@staticmethod
+	def format(d):
+		return datetime.datetime.strftime(d, Datetime.DATE_FORMAT)
+
+	@staticmethod
+	def format_time(d):
+		return datetime.datetime.strftime(d, Datetime.TIME_FORMAT)
+
+	@staticmethod
+	def parse(d: str):
+		return date_parser.parse(d)
