da90a4d6007770d40998c0f570f2263d78eadb2c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-14 11:12:08 +0300

    [twoopt:cli] Ref | Generalized impl.

--- twoopt/cli.py
+++ twoopt/cli.py
@@ -23,17 +23,9 @@ class RandomGenerator:
 			for prod in ut.radix_cartesian_product(self.schema.get_var_radix(var)):
 				yield (var, *prod,), random.uniform(0, self.var_bounds[var])
 
-		alpha_0 = random.uniform(0, 1)
-
-		yield ("alpha", 0), alpha_0
-		yield ("alpha", 1),  1 - alpha_0
-
 	def __post_init__(self):
 		self.schema = linsmat.Schema(None, self.schema_filename)
 		self.iter_state = None
-		assert list(self.schema.get_var_indices("psi")) == ["j", "i", "rho", "l"]
-		assert list(self.schema.get_var_indices("v")) == ["j", "rho", "l"]
-		assert list(self.schema.get_var_indices("phi")) == ["j", "rho", "l"]
 
 	def __iter__(self):
 		self.iter_state = iter(self._functor_iter_wrapper())
@@ -43,8 +35,9 @@ class RandomGenerator:
 		return next(self.iter_state)
 
 
-def generate_random(schema, psi_upper, phi_upper, v_upper, output):
-	generator = RandomGenerator(schema, ["psi", "v", "phi"], {"psi": psi_upper, "phi": phi_upper, "v": v_upper})
+def generate_random(schema, psi_upper, phi_upper, v_upper, x_eq_upper, output):
+	generator = RandomGenerator(schema, ["psi", "v", "phi", "alpha_1", "x_eq"],
+		{"psi": psi_upper, "phi": phi_upper, "v": v_upper, "alpha_1": 1.0, "x_eq": x_eq_upper})
 	ut.file_create_if_not_exists(output)
 	csv_data_provider = linsmat.PermissiveCsvBufferedDataProvider(output)
 
@@ -56,11 +49,12 @@ def generate_random(schema, psi_upper, phi_upper, v_upper, output):
 
 def _parse_arguments():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--generate-random", action="store_true", help="Gen. random data based on a schema")
+	parser.add_argument("--generate-random", action="store_true", help="Gen. random constraints for the linear programming task based on a schema")
 	parser.add_argument("--schema", type=str, help="Schema JSON file")
-	parser.add_argument("--psi-upper", type=float, help="Upper bound for psi")
-	parser.add_argument("--phi-upper", type=float, help="Upper bound for phi")
-	parser.add_argument("--v-upper", type=float, help="Upper bound for v")
+	parser.add_argument("--psi-upper", type=float, help="Upper bound for psi (upper bound in a le-constraint)")
+	parser.add_argument("--phi-upper", type=float, help="Upper bound for phi (upper bound in a le-constraint)")
+	parser.add_argument("--v-upper", type=float, help="Upper bound for v (upper bound in a le-constraint)")
+	parser.add_argument("--x-eq-upper", type=float, help="Upper bound for x_jlrho (right side in an eq-constraint)")
 	parser.add_argument("--output", type=str, default=ut.Datetime.format_time(ut.Datetime.today()) + ".csv")
 
 	return parser.parse_args()
