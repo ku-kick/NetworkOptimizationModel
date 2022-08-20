569b4eb2f1f6433e0b1b4d6f1b646e35334ae88e
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-20 15:22:52 +0300

    [test:test_linsolv_planner] Ref | Boilerplate

--- test/test_linsolv_planner.py
+++ test/test_linsolv_planner.py
@@ -18,16 +18,17 @@ class TestLinsolvPlanner(unittest.TestCase):
 		x_eq_upper = 10
 		cli.generate_random(TestLinsolvPlanner.SCHEMA_FILE_JSON, psi_upper, phi_upper, v_upper, x_eq_upper,
 			TestLinsolvPlanner.DATA_FILE_CSV)
+		self.data_provider = linsmat.PermissiveCsvBufferedDataProvider(csv_file_name=TestLinsolvPlanner.DATA_FILE_CSV)
+		self.schema = linsmat.Schema(filename=TestLinsolvPlanner.SCHEMA_FILE_JSON)
+		self.data_interface = linsmat.DataInterface(self.data_provider, self.schema)
 
 	def tearDown(self) -> None:
 		os.remove(TestLinsolvPlanner.DATA_FILE_CSV)
 
 	def test_init(self):
-		data_provider = linsmat.PermissiveCsvBufferedDataProvider(csv_file_name=TestLinsolvPlanner.DATA_FILE_CSV)
-		schema = linsmat.Schema(filename=TestLinsolvPlanner.SCHEMA_FILE_JSON)
-		data_interface = linsmat.DataInterface(data_provider, schema)
-		ls_planner = linsolv_planner.LinsolvPlanner(data_interface, schema)
-		self.assertTrue(schema.get_max_dec_from_indices("j", "rho", "l") == len(ls_planner.eq_lhs))
+
+		ls_planner = linsolv_planner.LinsolvPlanner(self.data_interface, self.schema)
+		self.assertTrue(self.schema.get_max_dec_from_indices("j", "rho", "l") == len(ls_planner.eq_lhs))
 		self.assertTrue(any(map(lambda i: len(i) == ls_planner.row_index.get_row_len(), ls_planner.eq_lhs)))
 		self.assertTrue(len(ls_planner.eq_lhs) == len(ls_planner.eq_rhs))
 		self.assertTrue(len(ls_planner.bnd) == ls_planner.row_index.get_row_len())
