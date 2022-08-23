515f865e1edf996d833f8dda6f3e69de106dc9d4
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-23 19:52:46 +0300

    [twoopt:linsolv_planner] Bug | Eq. constraint equation

--- twoopt/linsolv_planner.py
+++ twoopt/linsolv_planner.py
@@ -64,7 +64,8 @@ class LinsolvPlanner:
 
 		for i in range(self.schema.get_index_bound("i")):
 			if i != j:
-				stub[self.row_index.get_pos('x', i=i, j=j, rho=rho, l=l)] = 1
+				stub[self.row_index.get_pos('x', i=i, j=j, rho=rho, l=l)] = -1
+				stub[self.row_index.get_pos('x', i=j, j=i, rho=rho, l=l)] = 1
 
 		return stub
 
