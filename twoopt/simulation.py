fdf483caa76cacc8824c876c6595b77519a7a21f
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-30 22:43:29 +0300

    [twoopt:simulation] Impl | TransferOp

--- twoopt/simulation.py
+++ twoopt/simulation.py
@@ -11,10 +11,14 @@ class SimGlobal:
 
 
 @dataclass
+class Container:
+	amount: float
+
+
+@dataclass
 class Operation:
 	sim_global: SimGlobal
-	amount_input: float
-	amount_output: object
+	container_input: Container
 	amount_planned: float
 	indices_planned_plain: dict
 	proc_intensity_upper: float
@@ -23,9 +27,15 @@ class Operation:
 	proc_noise_type: bool = None  # None, "gauss"
 	amount_processed: float = 0.0
 
+	def amount_input(self):
+		return self.container_input.amount
+
 	def __post_init__(self):
 		if self.proc_intensity_lower is None:
 			self.proc_intensity_lower = 0
+
+	def amount_stash(self):
+		return 0
 
 	def amount_diff_planned(self):
 		return self.amount_planned - self.amount_processed
@@ -41,7 +51,14 @@ class Operation:
 			else:
 				return random.gauss(0, self.proc_intensity_lower / 4)
 
-	def amount_step(self):
+	def amount_processed_add(self, diff):
+		self.amount_processed += diff
+
+	def amount_input_add(self, diff):
+		self.container_input.amount += diff
+
+	def amount_proc_available(self):
+		""" How much to process during this step """
 		diff_planned = self.amount_diff_planned()
 
 		if self.proc_intensity_lower == 0.0:
@@ -52,16 +69,34 @@ class Operation:
 
 		amount_step_upper = (self.proc_intensity_upper * self.proc_intensity_fraction + self.noise())\
 							* self.sim_global.dt
-		amount_step = ut.clamp(diff_planned, amount_step_lower, amount_step_upper)
+		amount_step = ut.clamp(diff_planned, amount_step_lower, amount_step_upper)  # What is available due to technical limitations
+		amount_step = ut.clamp(amount_step, -self.amount_stash(), self.amount_input())  # What is available according to the amount of stashed / received
 
 		return amount_step
+
+
+class TransferOp(Operation):
+
+	def __post_init__(self):
+		Operation.__post_init__(self)
+		self.container_output: Container = None
+
+	def step(self):
+		assert self.container_output is not None
+		self._proc_step = self.amount_proc_available()
+		self.amount_input_add(-self._proc_step)
+
+	def step_transfer(self):
+		""" Flush out the stashed `_proc_step into the output container """
+		self.amount_processed_add(self._proc_step)
+		self.container_output.amount = self._proc_step
 
 
 class Simulation:
 	env: linsmat.Env
 
-	def make_ops_trasnfer(self):
+	def transfer_ops_init(self):
 		pass
 
-	def connect_ops_transfer(self):
+	def transfer_ops_connect(self):
 		pass
