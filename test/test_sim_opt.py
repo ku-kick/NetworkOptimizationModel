cf013b3c46d6521c9c64be2cf6b7e4818f699e69
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-20 11:30:07 +0300

    [test:test_sim_opt] Impl | TestSimOpt, carcass

---
+++ test/test_sim_opt.py
@@ -0,0 +1,54 @@
+import unittest
+import pathlib
+import sys
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent))
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / 'twoopt'))
+from twoopt import cli, linsmat, ut, linsolv_planner
+import functools
+import os
+import math
+from generic import Log
+import logging
+import ut
+
+
+log = ut.Log(file=__file__, level=ut.Log.LEVEL_VERBOSE)
+
+
+class TestSimOpt(unittest.TestCase):
+	__HERE = pathlib.Path(os.path.realpath(__file__)).parent
+	__SCHEMA_FILE = str((__HERE / "test_schema_3.json").resolve())
+	__CSV_OUTPUT_FILE = str((__HERE / "test_sim_output.csv").resolve())
+
+	def setUp(self) -> None:
+		psi_upper = 40
+		phi_upper = 30
+		v_upper = 70
+		x_eq_upper = 200
+		tl_upper = 500
+		mm_psi_upper = psi_upper / tl_upper
+		mm_phi_upper = phi_upper / tl_upper
+		mm_v_upper = v_upper / tl_upper
+		self.schema = linsmat.Schema(filename=self.__SCHEMA_FILE)
+		entry_nodes = list(map(lambda rho: dict(j=0, l=0, rho=rho), range(self.schema.get_index_bound("rho"))))
+
+		if not os.path.exists(self.__CSV_OUTPUT_FILE):
+			cli.generate_random(
+				schema=self.__SCHEMA_FILE,
+				psi_upper=psi_upper,
+				phi_upper=phi_upper,
+				v_upper=v_upper,
+				x_eq_upper=x_eq_upper,
+				mm_psi_upper=mm_psi_upper,
+				mm_phi_upper=mm_phi_upper,
+				mm_v_upper=mm_v_upper,
+				tl_upper=tl_upper,
+				entry_nodes=entry_nodes,
+				output=self.__CSV_OUTPUT_FILE
+			)
+		self.env = linsmat.Env.make_from_file(schema_file=self.__SCHEMA_FILE, storage_file=self.__CSV_OUTPUT_FILE,
+			row_index_variables=[])
+
+
+if __name__ == "__main__":
+	unittest.main()
