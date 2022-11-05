cb1f3766fc021dd76d895d6a121558798fce2371
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-05 12:23:06 +0300

    [twoopt:orchestration] Impl | ETL construction

--- twoopt/orchestration.py
+++ twoopt/orchestration.py
@@ -13,3 +13,32 @@ optimizers, and checking for stop conditions.
 import sim_opt
 import linsmat
 import linsolv_planner
+from dataclasses import dataclass
+import simulation
+
+@dataclass
+class VirtOpt:
+	"""
+	The '22 paper.
+	- Minimize loss (z)
+	- Maximize processing (g)
+	"""
+	N_ITERATIONS = 20
+	CONF_GA_GENE_SWAP_PERCENTAGE = .3
+	CONF_GA_POPULATION_SIZE = 30
+	CONF_GA_N_ITERATIONS = 10
+	CONF_GA_WORST_PERCENTAGE = .2  # Worst performers will be removed and replaced on each iteration
+	CONF_GA_SWAP_POPULATION_PERCENTAGE = .4  # Random individuals will be subjected to crossing
+
+	schema_path: str  # Path to .json schema file
+	storage_path: str  # Path to .csv storage file
+	conf_n_iterations = N_ITERATIONS
+
+	def __post_init__(self):
+		# Construct ETL entities
+		self.schema = linsmat.Schema(filename=self.schema_path)
+		self.csv_provider = linsmat.PermissiveCsvBufferedDataProvider(csv_file_name=self.storage_path)
+		self.csv_data_interface = linsmat.DataInterface(provider=self.csv_provider, schema=self.schema)
+		self.ram_provider = linsmat.DictRamDataProvider()
+		self.ram_data_interface = linsmat.DataInterface(provider=self.ram_provider, schema=self.schema)
+		self.ram_data_interface.update(self.csv_data_interface)  # Ensure consistency
