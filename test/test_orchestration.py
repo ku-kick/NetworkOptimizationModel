6bc6e98ecea8e3d9eb29c6a015385de9a9e41b8d
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-12 16:38:43 +0300

    [test:test_orchestration] Impl | Simple simulation optimization test

--- test/test_orchestration.py
+++ test/test_orchestration.py
@@ -29,6 +29,13 @@ class TestSimOpt(unittest.TestCase):
 		schema_file_json = str(pathlib.Path(__file__).parent / "ushakov.json")
 		virt_opt = orchestration.VirtOpt(schema_path=schema_file_json, storage_path=data_file_csv)
 
+	def test_optimization_simple(self):
+		config.cfg_set_test()
+		data_file_csv = str(pathlib.Path(__file__).parent / "test_opt_transfer_simple.csv")
+		schema_file_json = str(pathlib.Path(__file__).parent / "test_opt_transfer_simple.json")
+		virt_opt = orchestration.VirtOpt(schema_path=schema_file_json, storage_path=data_file_csv)
+		virt_opt.run()
+
 	def test_optimization(self):
 		config.cfg_set_test()
 		data_file_csv = str(pathlib.Path(__file__).parent / "ushakov.csv")
