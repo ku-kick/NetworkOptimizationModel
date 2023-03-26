5463e9649ba0bfa88da31eb11e58ffd25eba6709
DmitryMurashov <dmtr.murashov@gmail.com>
2023-03-26 13:11:20 +0300

    [:setup] Build |

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="twoopt",
+    packages=[
+        "twoopt",
+        "twoopt.data_processing",
+        "twoopt.optimization",
+    ],
+    include_package_data=True,
+    license="MIT",
+    description="Algorithms for network optimization",
+    long_description="",
+    author="Dmitry Murashov",
+    setup_requires=["wheel"],
+    install_requires=[
+        "pandas",
+        "numpy==1.23.2",
+        "scipy",
+        "simpy",
+        "pygal",
+        "CairoSVG",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Inependent",
+    ],
+    python_requires=">=3.7",
+    version="0.8.0",
+)
+
