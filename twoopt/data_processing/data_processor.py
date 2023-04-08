bd79bf6b1cffafa15bc537260b189ec8360a24cb
DmitryMurashov <dmtr.murashov@gmail.com>
2023-04-08 18:09:30 +0300

    [data_processing:data_processor] Impl | `Simulation` as a special sort of data processor

--- twoopt/data_processing/data_processor.py
+++ twoopt/data_processing/data_processor.py
@@ -29,8 +29,18 @@ class Solver(DataProcessor):
             data_interface: \
                 twoopt.data_processing.data_interface.DataInterfaceBase,
             schema: twoopt.data_processing.vector_index.Schema):
+        # TODO: does a solver really need a schema instance? Remove it
         DataProcessor.__init__(self, data_interface)
         self._schema = schema
 
     def get_schema(self):
         return self._schema
+
+
+class Simulation(DataProcessor):
+
+    def __init__(self, data_interface):
+        DataProcessor.__init__(self, data_interface=data_interface)
+
+    def run(self):
+        raise NotImplemented
