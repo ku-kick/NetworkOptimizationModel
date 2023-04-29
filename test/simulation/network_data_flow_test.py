d5fd94aac7234cfef932cf9bd61b3e8393c972db
DmitryMurashov <dmtr.murashov@gmail.com>
2023-04-29 15:08:23 +0300

    [simulation:network_data_flow_test] Enh | Absolute path's auto. inference

--- test/simulation/network_data_flow_test.py
+++ test/simulation/network_data_flow_test.py
@@ -1,6 +1,10 @@
+import pathlib
 import twoopt.data_processing.data_provider
 import twoopt.simulation.network_data_flow
 import unittest
+
+
+_THIS_PATH = pathlib.Path(__file__).resolve().parent
 
 
 class Test(unittest.TestCase):
@@ -8,7 +12,7 @@ class Test(unittest.TestCase):
     def test_ushakov_simulation(self):
         data_provider = twoopt.data_processing.data_provider\
             .PermissiveCsvBufferedDataProvider(
-            csv_file_name="network_data_flow_test/ushakov.csv")  # Network from "Ushakov, 2021"
+            csv_file_name=str(_THIS_PATH / "network_data_flow_test" / "ushakov.csv"))  # Network from "Ushakov, 2021"
         simulation = twoopt.simulation.network_data_flow.NetworkDataFlow(
             data_provider=data_provider)
         simulation.run()
