d075433540a7fe60c0af05e68300cd943b9bfc88
DmitryMurashov <dmtr.murashov@gmail.com>
2023-04-29 12:20:15 +0300

    [simulation:network_data_flow_test] Impl | Testing wrapper over legacy simulator

---
+++ test/simulation/network_data_flow_test.py
@@ -0,0 +1,18 @@
+import twoopt.data_processing.data_provider
+import twoopt.simulation.network_data_flow
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_ushakov_simulation(self):
+        data_provider = twoopt.data_processing.data_provider\
+            .PermissiveCsvBufferedDataProvider(
+            csv_file_name="network_data_flow_test/ushakov.csv")  # Network from "Ushakov, 2021"
+        simulation = twoopt.simulation.network_data_flow.NetworkDataFlow(
+            data_provider=data_provider)
+        simulation.run()
+
+
+if __name__ == "__main__":
+    unittest.main()
