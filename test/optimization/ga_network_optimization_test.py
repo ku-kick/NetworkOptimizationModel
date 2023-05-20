1151827be2e79ed7ceaea4a2bf09538d4592e09f
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-20 12:54:45 +0300

    [optimization:ga_network_optimization_test] Ref | Layed out ground for integrating the new config API

--- test/optimization/ga_network_optimization_test.py
+++ test/optimization/ga_network_optimization_test.py
@@ -7,6 +7,22 @@ import unittest
 _THIS_PATH = pathlib.Path(__file__).resolve().parent
 
 
+def _data_provider_init_ga_configs(data_provider):
+    values = {
+        "OPT_VIRT_GA_POPULATION_SIZE" : 3,
+        "OPT_VIRT_GA_SWAP_PERC_POPULATION" : 1.0,  # Fraction of individuals from the entire population that will be selected for crossing,
+        "OPT_VIRT_GA_REMOVE_PERC_POPULATION" : .6,  # % of population to be removed,
+        "OPT_VIRT_GA_N_ITERATIONS" : 2,
+        "OPT_VIRT_GA_N_ITERATIONS" : 2,
+        "OPT_VIRT_ORCHESTRATION_N_ITERATIONS" : 2,
+    }
+
+    for k, v in values.items():
+        data_provider.set_data(v, k)
+
+    return data_provider
+
+
 class GaNetworkOptimizationTest(unittest.TestCase):
     def test_run(self):
         data_provider = twoopt.data_processing.data_provider \
