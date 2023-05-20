71b3bfae4bcddf0f945193cd74448c460736d66f
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-20 15:38:00 +0300

    [optimization:ga_network_optimization_test] Ref | Removed changing test data before its use

--- test/optimization/ga_network_optimization_test.py
+++ test/optimization/ga_network_optimization_test.py
@@ -7,29 +7,12 @@ import unittest
 _THIS_PATH = pathlib.Path(__file__).resolve().parent
 
 
-def _data_provider_init_ga_configs(data_provider):
-    values = {
-        "OPT_VIRT_GA_POPULATION_SIZE" : 3,
-        "OPT_VIRT_GA_SWAP_PERC_POPULATION" : 1.0,  # Fraction of individuals from the entire population that will be selected for crossing,
-        "OPT_VIRT_GA_REMOVE_PERC_POPULATION" : .6,  # % of population to be removed,
-        "OPT_VIRT_GA_N_ITERATIONS" : 2,
-        "OPT_VIRT_GA_N_ITERATIONS" : 2,
-        "OPT_VIRT_ORCHESTRATION_N_ITERATIONS" : 2,
-    }
-
-    for k, v in values.items():
-        data_provider.set_data(v, k)
-
-    return data_provider
-
-
 class GaNetworkOptimizationTest(unittest.TestCase):
     def test_run(self):
         data_provider = twoopt.data_processing.data_provider \
             .PermissiveCsvBufferedDataProvider(
             csv_file_name=str( _THIS_PATH / "ga_network_optimization_test" \
             / "ushakov.csv"))  # Network from "Ushakov, 2021"
-        data_provider = _data_provider_init_ga_configs(data_provider)
         ga_network_optimization_solver = twoopt.optimization \
             .ga_network_optimization.GaNetworkOptimizationSolver(
             data_provider=data_provider)
